678631c8d87d82c24fe9981b5a24648b456423da
sherry <xieshiyi@xieshiyis-MacBook-Pro.local>
2016-09-11 17:31:12 +0800

    first commit

--- hello.js
+++ hello.js
@@ -20,4 +20,22 @@ $(function(){
       }
     }
   })
+
+/*双向绑定，渲染列表，处理用户输入*/
+//   new Vue({
+//   el:'#app',
+//   data:{
+//     message:'Hello Vue.js!',
+//     todos:[
+//       {text:'Learn JavaScript'},
+//       {text:'Learn Vue.js'},
+//       {text:'Build Something Awesome'}
+//     ]
+//   },
+//   methods:{
+//     reverseMessage:function(){
+//       this.message=this.message.split('').reverse().join('')
+//     }
+//   }
+// })
 });
